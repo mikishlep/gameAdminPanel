caf6b07fdfe8193e7c73011874fbd118f38e3955
mikishlep <gleb.tseilonsky@yandex.ru>
2025-09-06 19:35:43 +0500

    допы

---
+++ src/pages/Users.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>Пользователи</h1>
+    <p>Здесь будет таблица пользователей.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,16 +2,19 @@ import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
 
 import AuthForm from "../pages/AuthForm.vue";
-import Statistics from "../pages/Statistics.vue";
+import Dashboard from "../pages/Dashboard.vue";
+import Users from "../pages/Users.vue";
 
 export const RouteNames = {
     Auth: "Auth",
-    Statistics: "Statistics",
+    Dashboard: "Dashboard",
+    Users: "Users",
 } as const;
 
 const routes: RouteRecordRaw[] = [
     { path: "/auth", name: RouteNames.Auth, component: AuthForm },
-    { path: "/", name: RouteNames.Statistics, component: Statistics }
+    { path: "/", name: RouteNames.Dashboard, component: Dashboard },
+    { path: "/users", name: RouteNames.Users, component: Users },
 ];
 
 const router = createRouter({
